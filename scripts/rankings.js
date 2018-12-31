e01243e1d58a1af244f2f1d02bab926f49a6d4b9
Johan Riddersporre <johan.riddersporre@topgolf.com>
2018-12-31 22:37:24 +0100

    Initial commit

---
+++ scripts/rankings.js
@@ -0,0 +1,349 @@
+var rankingGraph = null;
+
+var graphOptions = {
+    chart: {
+        renderTo: 'rankings_graph',
+    },
+    plotOptions: {
+        areaspline:
+        {
+            fillOpacity : 0.3
+        }
+    },
+    title: {
+        text:"TopTracer TrueSkill(mod) Padel Ranking"
+    },
+    xAxis: {
+    },
+    yAxis: [{ 
+        labels: "",
+        title: {
+            text: "",
+        }
+        }]
+    // legend: {
+    //     enabled: false,
+    //     align: 'center',
+    //     verticalAlign: 'bottom',
+    //     floating: false,
+    //     backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    //};
+};
+
+function NormalDistribution(x, mu, sigma)
+{
+    return 1.0 / (sigma * Math.sqrt(2.0 * Math.PI)) * Math.exp(-(x-mu)*(x-mu) / (2.0 * sigma*sigma)) 
+}
+
+function NormalDistributionData(mu, sigma)
+{
+    var data = []
+    
+    var start = mu - sigma * 4.0; 
+    var end   = mu + sigma * 4.0; 
+    
+    var step = (end - start) / 30.0;
+    var x = start;
+    while(x <= end)
+    {
+        data.push([x, NormalDistribution(x, mu, sigma)]);
+        x += step;
+    }
+    return data;
+}
+
+
+function UpdateGraphData(rankings) 
+{
+    if(rankings == null)
+        return;
+
+    //var graph = $("#rankings_graph");
+    var series = []
+    var idx = 0
+    var plotLines = []
+    for(var i in rankings)
+    {
+        var r = rankings[i]
+        var serie = {
+            type: 'areaspline',
+            marker: {enabled:false},
+            name: r.Name,
+            zIndex: i,
+            data: NormalDistributionData(r.TrueSkill.mu, r.TrueSkill.sigma),
+        };
+        var plotLine = {
+            value: r.TrueSkill.ranking,
+            width: 1,
+            color: Highcharts.getOptions().colors[i],
+            zIndex: rankings.length + 1,
+            label: { 
+                text: r.Name,
+                style: {
+                    x: 10*i,
+                    y: 10*i,
+                    color: Highcharts.getOptions().colors[i]
+                }
+            }
+        }
+        plotLines.push(plotLine);
+        series.push(serie);  
+    }
+    graphOptions.series = series;
+    graphOptions.xAxis.plotLines = plotLines;
+    rankingGraph.update(graphOptions,true, true,false);
+}
+
+
+
+function UpdateRankings(rankings)
+{
+    if(rankings == null)
+        return;
+
+    var prevRanking = 0.0;
+    var pos = 1;
+    var table = $("#rankings_table");
+    table.empty();
+    for(var i = 0; i < rankings.length; i++)
+    {
+        var name = rankings[i].Name;
+        var ranking = rankings[i].TrueSkill.ranking;
+
+        if(ranking != prevRanking)
+            pos = i + 1;
+        table.append("<tr class=\"ranking\"><td>"+pos+"</td><td>"+name+"</td><td class=\"small\">"+ranking+"</td>");
+        prevRanking = ranking;
+    }
+}
+
+function nick_compare(a,b)
+{
+    if(a.nick < b.nick)
+        return -1;
+    if(a.nick > b.nick)
+        return 1;
+    return 0;
+}
+
+function UpdatePlayerSelection(resp)
+{
+    var selections = [$("#player1"), $("#player2"), $("#player3"), $("#player4")];
+
+    resp.sort(nick_compare);
+    for(var s in selections)
+    {
+        selections[s].empty();
+        for(var i in resp)
+        {
+            var r = resp[i];
+            selected = i == s ? "selected" : "";
+            selections[s].append("<option value=" +r.id+ " " +selected+">"+r.nick+"</option>");
+        }
+    }
+}
+
+function UpdateGames(games)
+{
+    if(games == null)
+        return;
+    var table = $("#last_games");
+    table.empty();
+    for(var i = games.length - 1; i >= 0 && i >= games.length - 5; i--)
+    {
+        var game = games[i];
+        var p1 = game.player1;
+        var p2 = game.player2;
+        var p3 = game.player3;
+        var p4 = game.player4;
+
+        var score1 = game.score1
+        var score2 = game.score2;
+
+        table.append("<tr class=\"games\"><td>"+p1+","+p2+"</td><td>"+p3+","+p4+"</td><td>"+score1+"-"+score2+"</td>");
+    }
+}
+
+function GetPlayers() {
+    $.getJSON("/data", { players: true }, function (resp, reqstatus) {
+        if (reqstatus == "success" && resp != null) {
+            UpdatePlayerSelection(resp);
+        }
+    });
+}
+
+
+function GetRankings() {
+    $.getJSON("/data", { rankings: true }, function (resp, reqstatus) {
+        if (reqstatus == "success" && resp != null) {
+            UpdateRankings(resp);
+            UpdateGraphData(resp)
+        }
+    });
+}
+
+function GetGames() {
+    $.getJSON("/data", { games: true }, function (resp, reqstatus) {
+        if (reqstatus == "success" && resp != null) {
+            UpdateGames(resp);
+        }
+    });
+}
+
+function AddGameButtonEnable()
+{
+    var p1 = parseInt($("#player1").val());
+    var p2 = parseInt($("#player2").val());
+    var p3 = parseInt($("#player3").val());
+    var p4 = parseInt($("#player4").val());
+
+    var s1 = parseInt($("#score1").val());
+    var s2 = parseInt($("#score2").val());
+
+    var disabled = isNaN(p1) || isNaN(p2) ||
+                   isNaN(p3) || isNaN(p4) ||
+                   isNaN(s1) || isNaN(s2) ||
+                   p1 == p2 || p1 == p3 || p1 == p4 ||
+                   p2 == p3 || p2 == p4 ||
+                   p3 == p4 ||
+                   s1 < 0 || s2 < 0 || s1 + s2 == 0;
+ 
+        
+    $("#add_game").prop("disabled", disabled);
+}
+
+function AddPlayerButtonEnable()
+{
+    var fn = $("#firstname").val();
+    var ln = $("#lastname").val();
+    var dn = $("#nick").val();
+
+    var disabled = (fn == null || fn == "" || 
+                    ln == null || ln == "" ||
+                    dn == null || dn == "" );
+
+    $("#add_player").prop("disabled", disabled);
+}
+
+
+function ValidateAndAddGame() 
+{
+    var p1 = parseInt($("#player1").val());
+    var p2 = parseInt($("#player2").val());
+    var p3 = parseInt($("#player3").val());
+    var p4 = parseInt($("#player4").val());
+
+    var s1 = parseInt($("#score1").val());
+    var s2 = parseInt($("#score2").val());
+
+    if(isNaN(p1) || isNaN(p2) ||
+       isNaN(p3) || isNaN(p4) ||
+       isNaN(s1) || isNaN(s2) ||
+       p1 == p2 || p1 == p3 || p1 == p4 ||
+       p2 == p3 || p2 == p4 ||
+       p3 == p4 ||
+       s1 < 0 || s2 < 0 || s1 + s2 == 0)
+    {
+        alert("Need to have 4 different players and valid scores");
+    }
+    else
+    {
+        $.ajax({
+            url : "/data/add_game",
+            type: "POST",
+            data: JSON.stringify(
+                { 
+                    "player1": p1, 
+                    "player2": p2,
+                    "player3": p3,
+                    "player4": p4,
+                    "score1": s1,
+                    "score2": s2,
+                    "americano": (s1 > 7 || s2 > 7) ? 1 : 0
+                }),
+            contentType: "application/json; charset=utf-8",
+            dataType   : "json",
+            success    : function(result,status,xhr){
+                alert(result.message);
+                if (status == "success")
+                { 
+                    GetGames();
+                    GetRankings();
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                //alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    }
+}
+
+function ValidateAndAddPlayer()
+{
+    var fn = $("#firstname").val();
+    var ln = $("#lastname").val();
+    var dn = $("#nick").val();
+
+    if(fn == "FirstName" || ln == "LastName" || dn == "DisplayName")
+    {
+        alert("Bad naming of player - Need to fill all names");
+    }
+    else 
+    {
+        $.ajax({
+            url : "/data/add_player",
+            type: "POST",
+            data: JSON.stringify(
+                { 
+                    "firstname": fn, 
+                    "lastname": ln,
+                    "nick": dn
+                }
+            ),
+            contentType: "application/json; charset=utf-8",
+            dataType   : "json",
+            success    : function(result,status,xhr){
+                alert(result.message);
+                if (status == "success")
+                {
+                    GetPlayers();
+                    GetRankings();                
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                //alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    }
+}
+
+
+function RankingsStartup()
+{
+    rankingGraph = Highcharts.chart(graphOptions);
+    GetRankings();
+    GetPlayers();
+    GetGames();
+    $("#add_player").click(function() {ValidateAndAddPlayer();});
+    $("#add_game").click(function() {ValidateAndAddGame();});
+    AddGameButtonEnable();
+    AddPlayerButtonEnable();
+
+    var arr = [ $("#player1"), $("#player2"), $("#player3"), $("#player4"), $("#score1"),$("#score2")]; 
+    for(var i in arr)
+    {
+        arr[i].change(function() { AddGameButtonEnable();});
+        arr[i].keyup(function() { AddGameButtonEnable();});
+        arr[i].click(function() { AddGameButtonEnable();});
+    }
+
+    arr = [ $("#firstname"), $("#lastname"), $("#nick") ];
+    for(var i in arr)
+    {
+        arr[i].change(function() { AddPlayerButtonEnable();});
+        arr[i].keyup(function() { AddPlayerButtonEnable();});
+    }
+
+}
---
+++ server.py
@@ -0,0 +1,303 @@
+import sys
+from twisted.web.server import Site, Request
+from twisted.web.resource import Resource
+from twisted.web.static import File
+from twisted.internet import reactor, endpoints
+from twisted.python import log
+import sqlite3
+import json
+import argparse
+import datetime as dt
+import pytz
+
+
+parser = argparse.ArgumentParser(description='Log data from an ARITERM Biomatic 20i to a sqlite database')
+parser.add_argument("-db", "--database", default="padel_ranking.db",  help="The sqlite database file (default: %(default)s)")
+parser.add_argument("-p", "--port", default=8880,  type=int, help="Webserver port (default: %(default)i)")
+parser.add_argument("-v", "--verbose", action='store_true',  help="verbose logging of calls")
+
+args = parser.parse_args()
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+##############################################
+##### DATABASE HANDLING
+##############################################
+def GetNextPlayerId(conn):
+    v = conn.execute('SELECT MAX(id) as ID FROM players').fetchone()
+    if v == None or v['ID'] == None:
+        return 1
+    return v['ID'] + 1
+
+def GetNextGameId(conn):
+    v = conn.execute('SELECT MAX(id) as ID FROM games').fetchone()
+    if v == None or v['ID'] == None:
+        return 1
+    return v['ID'] + 1
+
+def AddGame(conn, p1, p2, p3, p4, score1, score2, americano = False):
+    id1 = p1 if type(p1) is int else GetPlayerId(conn, p1)
+    id2 = p2 if type(p2) is int else GetPlayerId(conn, p2)
+    id3 = p3 if type(p3) is int else GetPlayerId(conn, p3)
+    id4 = p4 if type(p4) is int else GetPlayerId(conn, p4)
+    if id1 != None and id2 != None and id3 != None and id4 != None:  
+        nextId = GetNextGameId(conn)
+        time = dt.datetime.utcnow().replace(microsecond=0,tzinfo=pytz.utc).isoformat()
+        conn.execute("INSERT INTO games VALUES (?,?,?,?,?,?,?,?,?)", (nextId, id1, id2, id3, id4, score1, score2, 0 if americano == False else 1, time))
+        conn.commit()
+        return ("Added game with id {}   {}-{}   {}".format(nextId, score1, score2, "Americano" if americano else "" ))
+    return None
+
+def DeleteGame(conn, ids):
+    for id in ids:
+        conn.execute("DELETE FROM games WHERE id = ?", (id,))
+    conn.commit()
+    print("Deleted game with id {}".format(id))
+
+def DeleteAllGames(conn):
+    conn.execute("DELETE FROM games")
+    conn.commit()
+
+def IsNickUnique(conn, nick):
+    val = conn.execute("SELECT * FROM players WHERE nick = ?",(nick,)).fetchone()
+    if val == None:
+        return True
+    print("!!!! player already exists")
+    print(val)
+    return False
+
+def DeletePlayer(conn, nick):
+    conn.execute("DELETE FROM players WHERE nick = ?",(nick,))
+    conn.commit()
+
+def AddPlayer(conn, firstname, lastname, nick):
+    if(IsNickUnique(conn, nick)):
+        nextId = GetNextPlayerId(conn)
+        conn.execute("INSERT INTO players VALUES (?,?,?,?)", (nextId, firstname, lastname, nick))
+        conn.commit()
+        print("{} {}  added with nick '{}'".format(firstname, lastname, nick))
+        return True
+    return False
+
+def GetPlayerId(conn, nick):
+    v = conn.execute("SELECT id from players where nick = ?",(nick,)).fetchone()
+    if(v == None):
+        return None
+    return v['id']  
+
+def GetIdPlayerNameDict(conn, showNick = True):
+    vals = conn.execute("SELECT * from players").fetchall()
+    rats = {}
+    for p in vals:
+        rats[p['id']] = p['nick'] if showNick else p['firstname'] + " " + p["lastname"]
+    return rats
+
+def GetAllPlayers(conn):
+    return conn.execute("SELECT * from players").fetchall()
+
+def GetAllGames(conn):
+    return conn.execute("SELECT * from games").fetchall()
+
+def GetAllGamesWithNames(conn):
+    games = conn.execute("SELECT * from games").fetchall()
+    players = GetIdPlayerNameDict(conn)
+    for g in games:
+        g['player1'] = players[g['player1']]
+        g['player2'] = players[g['player2']]
+        g['player3'] = players[g['player3']]
+        g['player4'] = players[g['player4']]
+    return games
+
+#######################
+
+
+##############################################
+##### TRUE SKILL METHODS
+##############################################
+import trueskill as ts
+import itertools
+import math
+
+def GetSortedRanking_(players):
+    rankings = {}
+    for p in players:
+        rankings[p] = (ts.expose(players[p]), players[p])
+    return sorted(rankings.iteritems(), key=lambda (k,v): (v[0],k), reverse=True)
+
+def GetRanking(conn, players, print_ranking):
+    playerNames = GetIdPlayerNameDict(conn)
+    r = []
+    
+    if print_ranking:
+        print "---------------------"
+    
+    prevRankingPoint = 1000000.
+    pos = 1
+    
+    for p in GetSortedRanking_(players):
+        name = playerNames[p[0]]
+        if print_ranking:
+            rankingPoint = p[1][0]
+            if abs(rankingPoint - prevRankingPoint) > 1e-6:
+                pos = len(r) + 1
+            print("{}.\t{:10}   \t{}".format(pos, name, rankingPoint))
+            prevRankingPoint = rankingPoint
+        r.append((name, p[1]))
+    if print_ranking:
+        print "---------------------"
+    return r
+            
+def win_probability(team1, team2):
+    delta_mu = sum(r.mu for r in team1) - sum(r.mu for r in team2)
+    sum_sigma = sum(r.sigma ** 2 for r in itertools.chain(team1, team2))
+    size = len(team1) + len(team2)
+    TS = ts.TrueSkill()
+    denom = math.sqrt(size * (TS.BETA * TS.BETA) + sum_sigma)
+    return TS.cdf(delta_mu / denom)
+
+def _GetPlace(p1, p2):
+    return 1 if p1 <= p2 else 0
+                     
+def flatten(d):
+    r = {}
+    for e in d:
+        for f in e:
+            r[f] = e[f]
+    return r
+
+def CreateTeam(players, p1, p2):
+    return flatten((GetPlayer(db,p1), GetPlayer(db,p2)))
+
+def PlayGame_(team1, team2, res1, res2, americano = False):
+    rating_groups = (team1, team2)
+    total = float(res1 + res2)
+    a = res1 / total
+    b = res2 / total
+    d = abs(res1 - res2) / total
+    
+    factor = 0.3 if americano else 0.1
+    TS = ts.TrueSkill(draw_probability  = 0.05 + d * factor)  
+    
+    results = [(_GetPlace(res1, res2), _GetPlace(res2, res1))]
+
+    for r in results:
+        rating_groups = TS.rate(rating_groups, ranks=r)
+    return rating_groups
+
+def PlayAmericano_(team1, team2, res1, res2):
+    return PlayGame_(team1, team2, res1, res2, True)
+
+def ComputeRatings(conn, verbose):
+    temp_players = GetAllPlayers(conn)
+    
+    playerNames = GetIdPlayerNameDict(conn)
+    
+    players = {}
+
+    for p in temp_players:
+        players[p['id']] = ts.Rating()
+    games = GetAllGames(conn)
+    
+    for g in games:
+        p = (g['player1'], g['player2'], g['player3'], g['player4'])
+        team1 = {p[0] : players[p[0]], p[1]: players[p[1]]}
+        team2 = {p[2] : players[p[2]], p[3]: players[p[3]]}
+        s = (g['score1'], g['score2'])
+        am = g['gametype'] == 1
+        newRatings = flatten(PlayGame_(team1, team2, s[0], s[1], am))
+
+        if verbose:
+            print("({}, {})  vs ({}, {})    {}-{}   {}".format(playerNames[p[0]], playerNames[p[1]],
+                                                      playerNames[p[2]], playerNames[p[3]], s[0], s[1], "Americano" if am else "" ))
+        for t in newRatings:
+            if verbose:
+                diffMu = newRatings[t].mu - players[t].mu
+                diffRank = ts.expose(newRatings[t]) - ts.expose(players[t])
+                print("\t{}: mu:{}  rank:{}".format(playerNames[t], diffMu, diffRank)) 
+            players[t] = newRatings[t]
+    
+    return players
+
+#######################
+
+class DataFetching(Resource):
+    isLeaf = True
+
+    def __init__(self, databaseName):
+        print("Open database " + databaseName)
+        self.db = sqlite3.connect(databaseName)
+        self.db.row_factory = dict_factory
+
+    def __del__(self):
+        self.db.close()
+
+
+    def render_GET(self, request):
+        if len(request.args) < 1: return
+        request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+        request.setHeader(b'Access-Control-Allow-Origin', b'*')
+        if args.verbose:
+            print(request.args)
+        if request.args.get(b'players') != None:
+            request.setResponseCode(200)
+            return json.dumps(GetAllPlayers(self.db)).encode("utf8")
+
+        if request.args.get(b'games') != None:
+            request.setResponseCode(200)
+            return json.dumps(GetAllGamesWithNames(self.db)).encode("utf8")
+
+        if request.args.get(b'rankings') != None:
+            request.setResponseCode(200)
+            d = []
+            for r in GetRanking(self.db,ComputeRatings(self.db, False),True):
+                d.append( { "Name": r[0],  "TrueSkill" : { "ranking": r[1][0], "mu": r[1][1].mu, "sigma": r[1][1].sigma}})
+            return json.dumps(d).encode("utf8")
+        return ""
+
+    def render_POST(self, request):
+        if request.uri == "/data/add_player":
+            inp = request.content.read()
+            content = json.loads(inp)
+            if AddPlayer(self.db, content['firstname'], 
+                         content['lastname'], content['nick']):
+                request.setResponseCode(200)
+                return json.dumps({ "success": 1, "message": "{} {} added with nick '{}'".format(content['firstname'], content['lastname'], content['nick'])}).encode("utf8")
+            else:
+                return json.dumps({"success": 0, "message": "ERROR - Nick '{}' is not unique".format(content['nick'])}).encode("utf8")
+
+        if request.uri == "/data/add_game":
+            inp = request.content.read()
+            content = json.loads(inp)
+            m = AddGame(self.db, 
+                        content['player1'], content['player2'],
+                        content['player3'], content['player4'], 
+                        content['score1'], content['score2'],
+                        content['americano'])
+            if m != None:
+                request.setResponseCode(200)
+                return json.dumps({ "success": 1, "message": m}).encode("utf8")
+            else:
+                return json.dumps({"success": 0, "message": "Could not add game - Players not unique"}).encode("utf8")
+
+
+def main():
+    if args.verbose:
+        log.startLogging(sys.stdout)
+    root = Resource()
+    root.putChild(b"web", File("./web"))
+    root.putChild(b"scripts", File("./scripts"))
+    root.putChild(b"styles", File("./styles"))
+    root.putChild(b"data",  DataFetching(args.database))
+
+    factory = Site(root)
+    endpoint = endpoints.TCP4ServerEndpoint(reactor, args.port)
+    endpoint.listen(factory)
+    reactor.run()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
